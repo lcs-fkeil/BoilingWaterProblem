b0759d77316c0d3d9c864be94ef421e142bbce07
Franka Keil <fkeil@lcs.on.ca>
2022-02-15 11:18:43 -0500

    Initial Commit

---
+++ BoilingWaterProblem/BoilingWaterProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilingWaterProblemApp.swift
+//  BoilingWaterProblem
+//
+//  Created by Franka Keil on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BoilingWaterProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
