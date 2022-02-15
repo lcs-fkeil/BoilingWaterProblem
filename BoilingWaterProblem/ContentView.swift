2c867b5a9e4697c7edc953c8c9b112e85bad419c
Franka Keil <fkeil@lcs.on.ca>
2022-02-15 11:57:06 -0500

    Got the app to work with a slider and feedback on the place as well as the pressure.

--- BoilingWaterProblem/ContentView.swift
+++ BoilingWaterProblem/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    @State var temperature = 0.0
+    
+    // MARK: Computed properties
+    
+    var currentPressure: Double {
+        return (5 * temperature) - 400
+    }
+    
+    var feedback: String {
+        if currentPressure < 100 {
+            return "You are above sea level"
+        } else if currentPressure > 100 {
+            return "You are below sea level"
+        } else if currentPressure == 100 {
+            return "You are at sea level"
+        }
+        
+        return "I don't know where you are"
+        
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Slider(value: $temperature,
+                   in: 80...200,
+                   step: 1,
+                   label: {
+                Text("Temperature")
+            },
+                   minimumValueLabel: {
+                Text("80 °C")
+            },
+                   maximumValueLabel: {
+                Text("200 °C")
+            })
+            
+            Text("\(String(format: "%.1f", temperature)) °C")
+                .font(.largeTitle)
+                .bold()
+            
+            Text("Current Pressure:")
+                .font(.title2)
+            
+            Text("\(String(format: "%.1f", currentPressure)) kPa")
+                .font(.largeTitle)
+                .bold()
+            
+            Text (feedback)
+                .italic()
+                .padding()
+        }
+        .navigationTitle("Boiling water")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
